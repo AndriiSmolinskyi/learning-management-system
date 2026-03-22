58d083a68410c2ca22a8be1b43daf15018f5971b
AndriiiSmolinskyi <andrii.smo@cgsteam.io>
2026-03-22 22:30:27 +0200

    feat: Some

--- packages/backend/src/shared/utils/mail-templates/send-confirmation.template.ts
+++ packages/backend/src/shared/utils/mail-templates/send-confirmation.template.ts
@@ -24,24 +24,24 @@ export const sendEmailConfirmationTemplate = (
     <body style="font-family: 'Segoe UI', sans-serif; font-size: 13px;">
 
         <p>Dear ${body.name},</p>
-			<p>Welcome to Migdal Management Analytics — your all-in-one assets consolidation platform.</p>
+			<p>Welcome to LMS— your all-in-one assets consolidation platform.</p>
 			<p>Initial access details, you will be asked to change the password on first login.</p>
 			<ul>
 				<li>Username: ${body.email}</li>
 				<li>Password: ${body.password}</li>
 				<li>Link to platform: ${body.link}</li>
 			</ul>
-			<p>For any support, feel free to contact Migdal Management's back office directly.</p>
+			<p>For any support, feel free to contact LMS admin directly.</p>
 			<p>We are confident that our platform will be a valuable asset to you.</p>
 			<p>Thank you for your trust!</p>
 			<p>Sincerely,</p>
-			<p>Migdal Management Team</p>
+			<p>LMS</p>
     </body>
 
     </html>`
 
 	const mail = {
-		subject: 'Welcome to Migdal Management Analytics !',
+		subject: 'Welcome to lms !',
 		html,
 	}
 
